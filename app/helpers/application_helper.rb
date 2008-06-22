f1fd4a252c21f5e444b685c8457aa87edf75ff23
snagey <stefan41@gmail.com>
2008-06-22 18:28:26 +0100

    enabling new seo plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def title(site_name)
-    if site_name.class == String
-      "<h1>" + site_name + "</h1>"
-    elsif site_name.class == hash
-      site_name[:site_name]
-    else
-      site_name
-    end
-  end
+  #def title(site_name)
+  #  if site_name.class == String
+  #    "<h1>" + site_name + "</h1>"
+  #  elsif site_name.class == hash
+  #    site_name[:site_name]
+  #  else
+  #    site_name
+  #  end
+  #end
 
 end
