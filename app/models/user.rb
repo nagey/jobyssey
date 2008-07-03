573f8a21636d77d0aee0a6beaa5fd2884aff868e
Christina Nagey <ardita@windu.local>
2008-07-03 08:56:56 +0100

    merging conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,15 @@ class User < ActiveRecord::Base
   belongs_to :employer
   has_many :work_history_items
   
+  
+  validates_presence_of :first_name, :last_name
+  validates_acceptance_of :terms_of_service, :message => "You must agree to the Terms of Service to continue.", :on => :create
+  validates_confirmation_of :email, :password
+  validates_presence_of :password_confirmation, :if => :password_changed?
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_uniqueness_of :email, :message => "We've already got someone registered to this email address.  Please click on Forgot Password in the login bar if you've forgotten yours."
+  validates_length_of :password, :within => 6..20, :too_long => "pick a shorter password", :too_short => "pick a longer password"
+  
   def name
     first_name + " " + middle_names.chars.first + " " + last_name
   end  
