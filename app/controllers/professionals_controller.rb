ddd428fb5d3a966758f6a17887173d5c0c9d12e7
Christina Nagey <ardita@windu.local>
2008-07-17 10:21:18 +0100

    Fixing professionals controller

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,10 +1,8 @@
 class ProfessionalsController < ApplicationController
-
+ 
   layout 'index'
   
   def index
-<<<<<<< HEAD:app/controllers/professionals_controller.rb
-=======
     @cv = Cv.new
   end
   
@@ -13,11 +11,8 @@ class ProfessionalsController < ApplicationController
     @professional = Professional.new
     session[:cv] = @cv
     render :action => :index unless @cv.valid?
->>>>>>> c0965a58b5dee903c5648133067d90e370de9c0c:app/controllers/professionals_controller.rb
   end
-
-<<<<<<< HEAD:app/controllers/professionals_controller.rb
-=======
+ 
   def create
     @professional = Professional.new
     @professional.cv = session[:cv]
@@ -31,5 +26,4 @@ class ProfessionalsController < ApplicationController
     end
   end
   
->>>>>>> c0965a58b5dee903c5648133067d90e370de9c0c:app/controllers/professionals_controller.rb
 end
