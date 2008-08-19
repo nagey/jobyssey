2936ae1ad22f0d737724157a8960efb9e3826167
Christina Nagey <ardita@windu.local>
2008-08-19 12:49:23 +0100

    Added migrations for personal and employer questions and imporved codename migrations

--- db/migrate/20080817105223_create_prepositions.rb
+++ db/migrate/20080817105223_create_prepositions.rb
@@ -3,7 +3,10 @@ class CreatePrepositions < ActiveRecord::Migration
     create_table :prepositions do |t|
       t.string :preposition
     end
-    prepositions = ['above','across','after','along','among','around','at','before','behind','below','beneath','beside','between','beyond','near','over','toward','under','underneath','with']
+    prepositions = 
+['about', 'above', 'after','alongside', 'among', 'around', 'as', 'before', 'behind', 'beside', 'by', 'like', 'near','opposite', 'past','round', 'toward', 'with', 'ahead of', 'close to', 'far from', 'next to', 'in front of']
+   
+    
     prepositions.each {|p| w = Preposition.new; w.preposition = p; w.save}
   end
 
