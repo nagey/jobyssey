a9c3a66a90d15efcef552395960964b98b68fdd9
Christina Nagey <ardita@windu.local>
2008-08-24 12:01:17 +0100

    hooked up 3questions better

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,16 +1,22 @@
 class PortfolioController < ApplicationController
   
+  before_filter :authenticate
+  
   def links
-    @links
+    @attachment = Attachment.new
   end
   
   def add_a_link
-    @l = Attachment.new
-    @l.entity_type = "link"
-    @l.name = name
-    @l.url = url
-    @l.description = description
-    l.save
+    @attachment = Attachment.new params[:attachment]
+    @professional = Professional.find session[:user].id
+    if @attachment.save
+      @professional.attachments << @attachment
+      @professional.save
+      flash[:message] = "Link saved. please localize me."
+      redirect_to :action => :links
+    else
+      render :action => :links
+    end
   end  
 
 end
