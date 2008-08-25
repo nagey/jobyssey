3b3a4140d0764bf80a723dac1c8dc0feb76c95e7
unknown <Administrator@.(none)>
2008-08-25 16:23:03 +0100

    separated owlshawks

--- db/migrate/20080817105147_create_nouns.rb
+++ db/migrate/20080817105147_create_nouns.rb
@@ -3,7 +3,7 @@ class CreateNouns < ActiveRecord::Migration
     create_table :nouns do |t|
       t.string :noun
     end
-    nouns = ['Ants', 'Bears', 'Birds', 'Ducks', 'Fish', 'Horses', 'Rabbits', 'Spiders', 'Trees', 'Waves', 'Wind',  'Clouds', 'Mountains', 'Rivers', 'Buffaloes', 'Eagles', 'Owls' 'Hawks', 'Falcons', 'Forests', 'Trout', 'Oceans',  'Canoes', 'Wolves', 'Elephants', 'Hills', 'Lakes', 'Bulls', 'Cats', 'Seals', 'Sea Lions', 'Turtles', 'Giraffes', 'Lions', 'Tigers', 'Pumas', 'Jaguars', 'Zebras', 'Dolphins', 'Orcas', 'Starfish', 'Gazelle', 'Deer', 'Penguins', 'Pelicans', 'Swordfish']
+    nouns = ['Ants', 'Bears', 'Birds', 'Ducks', 'Fish', 'Horses', 'Rabbits', 'Spiders', 'Trees', 'Waves', 'Wind',  'Clouds', 'Mountains', 'Rivers', 'Buffaloes', 'Eagles', 'Owls', 'Hawks', 'Falcons', 'Forests', 'Trout', 'Oceans',  'Canoes', 'Wolves', 'Elephants', 'Hills', 'Lakes', 'Bulls', 'Cats', 'Seals', 'Sea Lions', 'Turtles', 'Giraffes', 'Lions', 'Tigers', 'Pumas', 'Jaguars', 'Zebras', 'Dolphins', 'Orcas', 'Starfish', 'Gazelle', 'Deer', 'Penguins', 'Pelicans', 'Swordfish']
     nouns.each {|n| w = Noun.new; w.noun = n; w.save }
   end
 
