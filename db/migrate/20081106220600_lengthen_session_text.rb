9cdd58e2f6582ce46c61b31b0b0b6bd65cf76c2f
Christina Nagey <ardita@windu.local>
2008-11-10 23:35:23 +0000

    Deleted old session migrations

--- db/migrate/20081106220600_lengthen_session_text.rb
+++
@@ -1,7 +0,0 @@
-class LengthenSessionText < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20081110232844_lengthen_session_text.rb
+++
@@ -1,8 +0,0 @@
-class LengthenSessionText < ActiveRecord::Migration
-  def self.up
-     execute "ALTER TABLE sessions MODIFY COLUMN text LONGTEXT"
-  end
-
-  def self.down
-  end
-end
