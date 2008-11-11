7506eddc5ef685ef0d7be5a96864a6022fafe05c
Christina Nagey <ardita@windu.local>
2008-11-11 09:14:13 +0000

    Small content change to professionals and non working employers navbar

--- app/controllers/skillset_controller.rb
+++ app/controllers/skillset_controller.rb
@@ -74,7 +74,7 @@ class SkillsetController < ApplicationController
 
  def start_job_posting_skills
     @job_posting = session[:job_posting]
-    @job_posting.metrics << @job_posting.job_specs.skills
+    @job_posting.metrics << @job_posting.job_specs.skills unless @job_posting.job_specs.nil?
     redirect_to :action => :define_job_posting_skills
   end
 
